d0722349696b2fb2d58dbcb240fef0f753e0cc5c
Altug Tatlisu <altug@bytus.io>
2026-02-27 18:18:22 -0500

    chore: remove temporary helper scripts

--- fix_cargo.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    if 'reqwest =' in line:
-        print('reqwest = { version = "0.12", default-features = false, features = ["json", "rustls-tls", "http2", "charset"] }')
-    else:
-        sys.stdout.write(line)
